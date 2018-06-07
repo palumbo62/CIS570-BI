d7ee26e1dc6085e536902cf98296abea23b0a509
Robert Palumbo <robert.palumbo@judicial.state.co.us>
2018-06-07 12:23:14 -0600

    Home Depot charts in ExcelAssignment/HomeDepotStockTrends. Additional Sample data R template (not really used for anything)

---
+++ Assignments/Week04/Session01/ExcelAssignment/HomeDepotStockTrends.R
@@ -0,0 +1,1 @@
+mydata <- read.csv("C:\\Users\\b88rlp\\Google Drive (robert.palumbo@ampsws.com)\\CSU\\2018Summer\\CIS570-BI\\Assignments\\Week04\\Session01\\ExcelAssignment\\EOD-HD.csv", header=TRUE)
